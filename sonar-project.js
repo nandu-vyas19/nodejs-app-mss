3bbeb928ad6bb3de3da507f8c8daedc6508e0408
NandithaGopisetti18 <97426458+NandithaGopisetti18@users.noreply.github.com>
2022-03-08 12:44:54 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://ec2-35-154-69-23.ap-south-1.compute.amazonaws.com:9000',
+  serverUrl: 'ec2-3-111-198-9.ap-south-1.compute.amazonaws.com:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
