1f3017be5148c75b910864d5b79734dc7297ab7a
NandithaGopisetti18 <97426458+NandithaGopisetti18@users.noreply.github.com>
2022-06-09 12:36:34 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://ec2-3-111-198-9.ap-south-1.compute.amazonaws.com:9000',
+  serverUrl: 'http://ec2-13-127-134-63.ap-south-1.compute.amazonaws.com:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': '5c2e067fbf0f29be84519fafc2be1dd94eab6c24',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'password',
+	    'sonar.password': 'nandu',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
